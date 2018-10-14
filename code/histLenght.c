65d8560d03a1f069d25da664d00c9d1dd983598f
MarvinMVargas <42704857+MarvinMVargas@users.noreply.github.com>
2018-10-13 21:06:36 -0500

    Add files via upload

--- code/histLenght.c
+++ code/histLenght.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 
 int main(){
-  int L[10],len = 0,c;
-  for(int i = 0;i < 10;i++)
+  int L[10],len = 0,i,j;
+  char c;
+  for( i = 0;i < 10;i++)
     L[i] = 0;
   while((c = getchar()) != EOF){
     if(c != ' ' && c != '\t' && c != '\n')
 	len++;
     else{
-      if(len <= 9)
-	L[(len - '0')-1]++;
+      if(len < 9)
+	L[len]++;
       else
 	L[9]++;
       len = 0;
     }
   }
   printf("\n");
-  for(int j = 1; j <= 10;j++){
+  for( j = 1; j <= 10;j++){
     printf("Len = %d\\\\",j);
-    for (int i = 0; i <= L[j]; i++)
+    for ( i = 0; i <= L[j]; i++)
       printf("|");
     printf("\n");
   }
